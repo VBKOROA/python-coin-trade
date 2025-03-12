5a19207559bc0ed66c79cd5315f6ca84fe8451c1
abko <abko121115@gmail.com>
2025-03-12 23:07:00 +0900

    refactor: 판매 및 구매 메서드 주석 추가 및 코드 가독성 향상

--- repos/action_log_repo.py
+++ repos/action_log_repo.py
@@ -9,24 +9,24 @@ class ActionLogRepo:
         self.__dbms = dbms
         
     def sell_coin(self, current_price, coin) -> Decimal:
-        market = coin['market']
-        amount = coin['amount']
-        # 수수료를 고려한 가격
-        price = (amount * Decimal(current_price) * self.FEE_RATE).quantize(Decimal('1'), rounding='ROUND_DOWN')
-        action = 'sell'
+        # 판매할 코인 정보를 이용하여 판매
+        market = coin['market'] # 코인 마켓 정보
+        amount = coin['amount'] # 코인 수량
+        price = (amount * Decimal(current_price) * self.FEE_RATE).quantize(Decimal('1'), rounding='ROUND_DOWN') # 판매 가격 계산 (수수료 적용)
+        action = 'sell' # 액션 타입
         query = """
             INSERT INTO action_log (market, amount, price, action)
             VALUES (%s, %s, %s, %s)
         """
-        self.__dbms.cursor.execute(query, (market, amount, price, action))
-        return price
+        self.__dbms.cursor.execute(query, (market, amount, price, action)) # DB에 판매 로그 기록
+        return price # 판매 가격 반환
     
     def buy_coin(self, market: str, current_price: int, price: Decimal) -> Decimal:
-        amount = price / Decimal(current_price) * self.FEE_RATE
-        action = 'buy'
+        amount = price / Decimal(current_price) * self.FEE_RATE # 구매 가능한 코인 수량 계산 (수수료 적용)
+        action = 'buy' # 액션 타입
         query = """
             INSERT INTO action_log (market, amount, price, action)
             VALUES (%s, %s, %s, %s)
         """
-        self.__dbms.cursor.execute(query, (market, amount, price, action))
-        return amount
+        self.__dbms.cursor.execute(query, (market, amount, price, action)) # DB에 구매 로그 기록
+        return amount # 구매한 코인 수량 반환
