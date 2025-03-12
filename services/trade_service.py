a902c4908781590b9ff60c6504110f62c30236c3
abko <abko121115@gmail.com>
2025-03-12 23:10:21 +0900

    refactor: execute_trade_logic 메서드에 주석 추가

--- services/trade_service.py
+++ services/trade_service.py
@@ -22,6 +22,12 @@ class TradeService:
         self.__conn = conn
         
     async def execute_trade_logic(self):
+        """
+            자동 매매 로직을 실행하는 메서드.
+            캔들 차트를 가져오고, 팔아야 할 코인이 있는지 확인하여 판매한다.
+            AI 모델을 통해 매매 결정을 수행하고, 결정에 따라 코인을 구매한다.
+        """
+        
         # 먼저 캔들 차트를 가져온다.
         candle_chart = await self.__upbit_client.fetch_candle_chart(self.__timeframe_config)
         
