e94bfd262a90511f4453f23bdd1c77081be39f4b
abko <abko121115@gmail.com>
2025-03-14 16:28:13 +0900

    feat: buy_and_sell_test 함수 추가 및 ActionService를 사용한 코인 구매/판매 테스트 구현

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 import asyncio
 from api.upbit_client import UpbitClient
+from models.dto.decision import Decision
+from repos.member_repo import MemberRepo
+from services.action_service import ActionService
 from services.llm_service import LLMService
+from settings.db_connection import DBMS
 from settings.singleton_pack import SingletonPack as sgtPack
 
 async def test_candle_and_gemini(upbit_client: UpbitClient, llm_service: LLMService):
@@ -12,10 +16,40 @@ async def test_candle_and_gemini(upbit_client: UpbitClient, llm_service: LLMServ
     
     print("결정:", decision)
     
+def buy_and_sell_test(action_service: ActionService, member_repo: MemberRepo, dbms: DBMS):
+    with dbms.get_session() as session:
+        member = member_repo.get_member_by_id(1, session)
+        
+        # 코인 구매 테스트
+        buy_decision = Decision({
+            "up_chance": 100,
+            "down_chance": 0,
+            "details": ""
+        })
+        buy_decision.set_current_price(10000)
+        buy_decision.set_market("KRW-BTC")
+        action_service.buy_coin(member, buy_decision, session)
+        session.commit()
+        
+        # 코인 판매 테스트
+        sell_decision = Decision({
+            "up_chance": 0,
+            "down_chance": 100,
+            "details": ""
+        })
+        sell_decision.set_current_price(10000)
+        sell_decision.set_market("KRW-BTC")
+        coin = member.coin
+        action_service.sell_coin(coin, sell_decision, session)
+        session.commit()
+        
+        print("코인 구매 및 판매 테스트 완료")
+    
 if __name__ == "__main__":
     s_pack = sgtPack()
     try:
-        asyncio.run(test_candle_and_gemini(s_pack.upbit_client, s_pack.llm_service))
+        # asyncio.run(s_pack.trade_service.execute_trade_logic(1))
+        buy_and_sell_test(s_pack.action_service, s_pack.member_repo, s_pack.dbms)
     except KeyboardInterrupt:
         print("프로그램이 종료되었습니다.")
     finally:
